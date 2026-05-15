6f576aca48c585f280195b8e1a9fff0610aab107
MKANE955 <hemmmiguez@gmail.com>
2026-05-15 14:43:34 -0300

    agrego PWA: manifest, service worker e iconos

---
+++ src/sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'ideas-v1';
+const ASSETS = [
+  '/notas-rapidas/',
+  '/notas-rapidas/index.html',
+  '/notas-rapidas/icon-192.png',
+  '/notas-rapidas/icon-512.png',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(ks =>
+      Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
